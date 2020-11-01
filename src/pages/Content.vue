98d891ffef673cf6857fdd5ba912addcc9eef93c
小组成员 <199227963@qq.com>
2020-11-01 15:20:30 +0800

    这是小组成员完成的模块

---
+++ src/pages/Content.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>我是小组成员完成的功能</div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Content from '../pages/Content.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // }
+    {
+      path: '/content',
+      name: 'Content',
+      component: Content
+    }
   ]
 })
